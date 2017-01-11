35509125bd16c29fa2808cbb37204c863d812015
Iliya Miroslavov Iliev <i.miroslavov@gmail.com>
2017-01-11 20:37:23 +0200

    "simple" theme modifications

--- themes/simple.sh
+++ themes/simple.sh
@@ -12,11 +12,11 @@ DEFAULT_PS1="\u@\h:\w"
 #%D% - deleted files
 #%C% - conflicts
 
-PROMPT_FORMAT="[%B%%-%%+%][÷%S%±%M%+%U%·%A%-%D%×%C%]"
+PROMPT_FORMAT="[%B%%-%%+%][S:%S%,M:%M%,U:%U%,A:%A%,D:%D%,C:%C%]"
 
 #%+% - commits ahead format
-PROMPT_FORMAT_AHEAD="↑%N%"
+PROMPT_FORMAT_AHEAD="+%N%"
 
 #%-% - commits behind format
-PROMPT_FORMAT_BEHIND="↓%N%"
+PROMPT_FORMAT_BEHIND="-%N%"
 
