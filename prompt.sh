046c5f4ecbe709dadff16a09ee3ca4b57462374c
Iliya Miroslavov Iliev <i.miroslavov@gmail.com>
2017-01-12 19:44:24 +0200

    Minor optimization

--- prompt.sh
+++ prompt.sh
@@ -10,36 +10,36 @@ CLEAN_PS1=$(echo "$DEFAULT_PS1" | sed "s/ *//g")
 
 generate_prompt() {
 	local PROMPT=$PROMPT_FORMAT
-	local STATUS=$(git status -s --porcelain)
-	
-	PROMPT=$(echo $PROMPT | sed "s/%B%/$(git branch | grep -e '^\*' | awk '{print $2}')/")
-	PROMPT=$(echo $PROMPT | sed "s/%M%/$(printf -- '%s\n' "${STATUS[@]}" | egrep -v '^\?\?' | grep -E '^.{0,1}[RM]' | wc -l)/")
-	PROMPT=$(echo $PROMPT | sed "s/%A%/$(printf -- '%s\n' "${STATUS[@]}" | egrep -v '^\?\?' | grep -E '^.{0,1}A' | wc -l)/")
-	PROMPT=$(echo $PROMPT | sed "s/%D%/$(printf -- '%s\n' "${STATUS[@]}" | egrep -v '^\?\?' | grep -E '^.{0,1}D' | wc -l)/")
-	PROMPT=$(echo $PROMPT | sed "s/%U%/$(printf -- '%s\n' "${STATUS[@]}" | egrep '^\?\?' | wc -l)/")
-	PROMPT=$(echo $PROMPT | sed "s/%C%/$(printf -- '%s\n' "${STATUS[@]}" | egrep -v '^\?\?' | grep -E '^.{0,1}U' | wc -l)/")
+	local STATUS=$(git status --branch --porcelain)
+	local BRANCH_INFO=$(head -1  <<< $STATUS)
+
+	PROMPT=$(echo $PROMPT | sed "s/%B%/$(echo $BRANCH_INFO | awk '{print $2}' | cut -d'.' -f1 )/")
+	PROMPT=$(echo $PROMPT | sed "s/%M%/$(egrep -v '^\?\?' <<< $STATUS | grep -E '^.{0,1}[RM]' | wc -l)/")
+	PROMPT=$(echo $PROMPT | sed "s/%A%/$(egrep -v '^\?\?' <<< $STATUS | grep -E '^.{0,1}A' | wc -l)/")
+	PROMPT=$(echo $PROMPT | sed "s/%D%/$(egrep -v '^\?\?' <<< $STATUS | grep -E '^.{0,1}D' | wc -l)/")
+	PROMPT=$(echo $PROMPT | sed "s/%U%/$(egrep '^\?\?' <<< $STATUS | wc -l)/")
+	PROMPT=$(echo $PROMPT | sed "s/%C%/$(egrep -v '^\?\?' <<< $STATUS | grep -E '^.{0,1}U' | wc -l)/")
 	PROMPT=$(echo $PROMPT | sed "s/%S%/$(git stash list | wc -l)/")
 
-	local STATUS=$(git rev-list --count --left-right "@{upstream}"...HEAD)
-
-	PROMPT=$(echo $PROMPT | sed "s/%+%/$(get_branch_status $(echo $STATUS | awk '{print $2}') $PROMPT_FORMAT_AHEAD)/")
-	PROMPT=$(echo $PROMPT | sed "s/%-%/$(get_branch_status $(echo $STATUS | awk '{print $1}') $PROMPT_FORMAT_BEHIND)/")
+	PROMPT=$(echo $PROMPT | sed "s/%+%/$(get_branch_status "$BRANCH_INFO" $PROMPT_FORMAT_AHEAD 'ahead')/")
+	PROMPT=$(echo $PROMPT | sed "s/%-%/$(get_branch_status "$BRANCH_INFO" $PROMPT_FORMAT_BEHIND 'behind')/")
 
 	echo "$PROMPT"
 }
 
 get_branch_status() {
 	local RESULT=""
+	local COUNT=$(echo $1 | sed -n "s/.*$3\s\([0-9]\).*/\1/p")
 
-	if [ $1 -gt 0 ]; then
-		RESULT=$(echo $2 | sed "s/%N%/$1/")
+	if [ ${#COUNT} -gt 0 ]; then
+		RESULT=$(echo $2 | sed "s/%N%/$COUNT/")
 	fi
 	
 	echo $RESULT
 }
 
 create_prompt() {
-	if [ -d ".git" ] && $(git rev-parse --is-inside-work-tree 2>/dev/null); then
+	if $(git rev-parse --is-inside-work-tree 2>/dev/null); then
 		local FORMAT=""
 		if [ $PROMPT_MILTILINE -eq 1 ]; then
 			FORMAT="$(generate_prompt)\n$DEFAULT_PS1\\$ "
