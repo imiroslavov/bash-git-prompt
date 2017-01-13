4a7f5cd16a041b9457a3fd8b692915aeddf7065b
Iliya Miroslavov Iliev <i.miroslavov@gmail.com>
2017-01-13 16:21:19 +0200

    Accurate branch name

--- prompt.sh
+++ prompt.sh
@@ -13,7 +13,7 @@ generate_prompt() {
 	local STATUS=$(git status --branch --porcelain)
 	local BRANCH_INFO=$(printf -- '%s\n' "${STATUS[@]}" | head -1)
 
-	PROMPT=$(echo $PROMPT | sed "s/%B%/$(echo $BRANCH_INFO | awk '{print $2}' | cut -d'.' -f1 )/")
+	PROMPT=$(echo $PROMPT | sed "s/%B%/$(get_branch_name "$BRANCH_INFO")/")
 	PROMPT=$(echo $PROMPT | sed "s/%M%/$(printf -- '%s\n' "${STATUS[@]}" | egrep -v '^\?\?' | grep -E '^.{0,1}[RM]' | wc -l)/")
 	PROMPT=$(echo $PROMPT | sed "s/%A%/$(printf -- '%s\n' "${STATUS[@]}" | egrep -v '^\?\?' | grep -E '^.{0,1}A' | wc -l)/")
 	PROMPT=$(echo $PROMPT | sed "s/%D%/$(printf -- '%s\n' "${STATUS[@]}" | egrep -v '^\?\?' | grep -E '^.{0,1}D' | wc -l)/")
@@ -21,21 +21,35 @@ generate_prompt() {
 	PROMPT=$(echo $PROMPT | sed "s/%C%/$(printf -- '%s\n' "${STATUS[@]}" | egrep -v '^\?\?' | grep -E '^.{0,1}U' | wc -l)/")
 	PROMPT=$(echo $PROMPT | sed "s/%S%/$(git stash list | wc -l)/")
 
-	PROMPT=$(echo $PROMPT | sed "s/%+%/$(get_branch_status "$BRANCH_INFO" $PROMPT_FORMAT_AHEAD 'ahead')/")
-	PROMPT=$(echo $PROMPT | sed "s/%-%/$(get_branch_status "$BRANCH_INFO" $PROMPT_FORMAT_BEHIND 'behind')/")
+	PROMPT=$(echo $PROMPT | sed "s/%+%/$(get_branch_status "$BRANCH_INFO" "$PROMPT_FORMAT_AHEAD" 'ahead')/")
+	PROMPT=$(echo $PROMPT | sed "s/%-%/$(get_branch_status "$BRANCH_INFO" "$PROMPT_FORMAT_BEHIND" 'behind')/")
 
 	echo "$PROMPT"
 }
 
 get_branch_status() {
 	local RESULT=""
-	local COUNT=$(echo $1 | sed -n "s/.*$3\s\([0-9]\).*/\1/p")
+	local COUNT=$(echo $1 | sed -n "s/.*$3 \([0-9]\).*/\1/p")
 
 	if [ ${#COUNT} -gt 0 ]; then
 		RESULT=$(echo $2 | sed "s/%N%/$COUNT/")
 	fi
 	
 	echo $RESULT
+}
+
+get_branch_name() {
+	local BRANCH=$(echo $1 | sed -n 's/## \(.*\)\.\.\.\(.*\)/\1/p')
+
+	if [[ ! $BRANCH ]]; then
+		BRANCH=$(echo $1 | sed -n 's/## HEAD (\(.*\))/\1/p')
+	fi
+
+	if [[ ! $BRANCH ]]; then
+		BRANCH=$(echo $1 | sed -n 's/## Initial commit on \(.*\)/\1/p')
+	fi
+
+	echo $BRANCH
 }
 
 create_prompt() {
